4c8caf601ae986157b29380a3e1b06806987cc90
BeRay31 <beray.prof.if@gmail.com>
2021-09-24 23:00:57 +0700

    day14

--- code-leetcode/src/main/java/beray/leetcode/AlgorithmStudies/Day14/ReverseBits.java
+++ code-leetcode/src/main/java/beray/leetcode/AlgorithmStudies/Day14/ReverseBits.java
@@ -1,5 +1,19 @@
 package beray.leetcode.AlgorithmStudies.Day14;
 // https://leetcode.com/problems/reverse-bits/
 public class ReverseBits {
-  
+  public int reverseBits(int n) {
+    int res = 0;
+    for(int i = 0; i < 32; i++) {
+      res <<= 1;
+      if ((n & 1) == 1) {
+        res+=1;
+      }
+      n = n >>> 1;
+    }
+    return res;
+  }
+  public static void main(String[] args) {
+    ReverseBits tb = new ReverseBits();
+    System.out.println(tb.reverseBits(-3));
+  }
 }
--- code-leetcode/src/main/java/beray/leetcode/AlgorithmStudies/Day14/SingleNumber.java
+++ code-leetcode/src/main/java/beray/leetcode/AlgorithmStudies/Day14/SingleNumber.java
@@ -1,5 +1,12 @@
 package beray.leetcode.AlgorithmStudies.Day14;
 // https://leetcode.com/problems/single-number/
+// Bitwise XOR N ^ N = 0, 0 ^ N = N
 public class SingleNumber {
-  
+  public int singleNumber(int[] nums) {
+    int ans = 0;
+    for (int i = 0; i < nums.length; i++) {
+      ans ^= nums[i];
+    }
+    return ans;
+  }
 }
