97d47a16b55d1fb2bd7104f9d504cb987577e2b0
BeRay31 <beray.prof.if@gmail.com>
2021-09-24 18:36:56 +0700

    day13

--- code-leetcode/src/main/java/beray/leetcode/AlgorithmStudies/Day13/NumberOf1Bits.java
+++ code-leetcode/src/main/java/beray/leetcode/AlgorithmStudies/Day13/NumberOf1Bits.java
@@ -1,5 +1,13 @@
 package beray.leetcode.AlgorithmStudies.Day13;
 // https://leetcode.com/problems/number-of-1-bits/
 public class NumberOf1Bits {
-  
+  // Basic bit shifting
+  public int hammingWeight(int n) {
+    int sumOne = 0;
+    while (n != 0) {
+      sumOne = sumOne + (n & 1);
+      n = n >>> 1;
+    }
+    return sumOne;
+  }
 }
--- code-leetcode/src/main/java/beray/leetcode/AlgorithmStudies/Day13/PowerOfTwo.java
+++ code-leetcode/src/main/java/beray/leetcode/AlgorithmStudies/Day13/PowerOfTwo.java
@@ -1,5 +1,13 @@
 package beray.leetcode.AlgorithmStudies.Day13;
 // https://leetcode.com/problems/power-of-two/
 public class PowerOfTwo {
-  
+  public boolean isPowerOfTwo(int n) {
+    while (n != 0) {
+      if ((n & 1) != 1) {
+        return false;
+      }
+      n = n >>> 1;
+    }
+    return true;
+  }
 }
